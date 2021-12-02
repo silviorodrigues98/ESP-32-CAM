8755487f4bb284e1495982deb1b2193892294306
SilvioCorrea <silviorodrigues98@hotmail.com>
2021-12-02 11:18:06 -0300

    Atualizações de códigos

--- Onboard_Bluetooh_Commands/src/main.cpp
+++ Onboard_Bluetooh_Commands/src/main.cpp
@@ -1,5 +1,4 @@
 #include "BluetoothSerial.h"
-#include <string.h>
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
@@ -11,47 +10,80 @@ BluetoothSerial SerialBT;
 
 #define LED_BUILTIN  33
 #define LED_FLASH 4
-#define GPIO 2
-char* ledB; 
-char* ledF;
-char* gpio;
+#define GPIOOUT 2
+#define GPIOIN 15
+char command;
+char* statusLED = "LED Apagado ";
+char* statusFLASH = "FLASH Apagado ";
+char* statusGPIOOUT = "GPIO2OUT Desativado ";
+char* statusGPIOIN = "GPIO15IN Desativado";
 
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  SerialBT.begin("ESP32-CAM Silvio"); //Bluetooth device name
+  Serial.println("\nBluetooth ativado, pode parear!!!");
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(LED_FLASH, OUTPUT);
-  pinMode(GPIO, OUTPUT);
+  pinMode(GPIOOUT, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
-   ledB  = (char*)SerialBT.read();
-   ledF  = (char*)SerialBT.read();
-   gpio  = (char*)SerialBT.read();
+  command  = (char)SerialBT.read();
+  if (command == 'H') {
+    SerialBT.println("COMANDOS:\nA/B = LED \nC/D = FLASH\nD/E = \nGPIO2OUT = E/F\nGPIO15IN = G/H\nS = STATUS\nH = HELP");
+  }
+  if (command == 'S') {
+    SerialBT.println(statusLED);
+    SerialBT.println(statusFLASH);
+    SerialBT.println(statusGPIOOUT);
+    SerialBT.println(statusGPIOIN);
+  }
   if (Serial.available()) {
     SerialBT.write(Serial.read());
   }
   if (SerialBT.available()) {
     Serial.write(SerialBT.read());
-    if(ledB  == "Ligar LED") {
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-    if(ledB  == "Desligar LED") {
+    if (command  == 'A') {
       digitalWrite(LED_BUILTIN, LOW);
+      statusLED = "LED Aceso";
+      SerialBT.println(statusLED);
     }
-    if(ledF  == "Ligar FLASH") {
+    if (command  == 'B') {
+      digitalWrite(LED_BUILTIN, HIGH);
+      statusLED = "LED Apagado";
+      SerialBT.println(statusLED);
+    }
+    if (command  == 'C') {
       digitalWrite(LED_FLASH, HIGH);
+      statusFLASH = "FLASH Aceso";
+      SerialBT.println(statusFLASH);
     }
-    if(ledF  == "Desligar FLASH") {
+    if (command  == 'D') {
       digitalWrite(LED_FLASH, LOW);
+      statusFLASH = "FLASH Apagado";
+      SerialBT.println(statusFLASH);
     }
-    if(gpio  == "Ligar GPIO") {
-      digitalWrite(GPIO, HIGH);
+    if (command  == 'E') {
+      digitalWrite(GPIOOUT, HIGH);
+      statusGPIOOUT = "GPIO2OUT Ativado";
+      SerialBT.println(statusGPIOOUT);
     }
-    if(gpio  == "Desligar GPIO") {
-      digitalWrite(GPIO, LOW);
+    if (command  == 'F') {
+      digitalWrite(GPIOOUT, LOW);
+      statusGPIOOUT = "GPIO2OUT Desativado";
+      SerialBT.println(statusGPIOOUT);
+    }
+    if (command == 'G') {
+      digitalWrite(GPIOIN, HIGH);
+      statusGPIOIN = "GPIO15IN Ativado";
+      SerialBT.println(statusGPIOIN);
+    }
+    if (command == 'H') {
+      digitalWrite(GPIOIN, LOW);
+      statusGPIOIN = "GPIO15IN Desativado";
+      SerialBT.println(statusGPIOIN);
     }
   }
   delay(20);
